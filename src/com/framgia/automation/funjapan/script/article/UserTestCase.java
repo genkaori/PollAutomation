947c9cc46f8d2ad63b3a378b43947e7c63af64bb
chuanhtuan <chu.anh.tuan@framgia.com>
2018-07-02 16:45:30 +0700

    sample

--- src/com/framgia/automation/funjapan/script/article/UserTestCase.java
+++ src/com/framgia/automation/funjapan/script/article/UserTestCase.java
@@ -1,5 +1,13 @@
 package com.framgia.automation.funjapan.script.article;
 
-public class UserTestCase {
+import org.testng.annotations.Test;
+
+import com.framgia.automation.funjapan.script.CommonTestCase;
+
+public class UserTestCase extends CommonTestCase {
+	@Test(priority=1, dataProvider="SetLogin")
+	public void testLogin(String email, String pass) {
+		testLogin(email, pass);
+	}
 
 }
