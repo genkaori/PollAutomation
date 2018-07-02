ad5a1af7615f2d188acdb450914ff5b981f4ae01
chuanhtuan <chu.anh.tuan@framgia.com>
2018-07-02 17:47:15 +0700

    gitignore

--- src/com/framgia/automation/funjapan/script/CommonTestCase.java
+++ src/com/framgia/automation/funjapan/script/CommonTestCase.java
@@ -7,23 +7,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 
 import com.framgia.automation.funjapan.util.Setting;
 import com.framgia.automation.funjapan.util.XLSHelper;
 
 public abstract class CommonTestCase {
-	private WebDriver driver = null;
+	 public WebDriver driver = null;
 
-	@BeforeMethod
+	@BeforeTest
 	public void beforeTest() {
 		System.setProperty(Setting.getSetting(Setting.WEBDRIVER), Setting.getSetting(Setting.WEBDRIVER_PATH));
 		driver = new ChromeDriver();
 		driver.get(Setting.getSetting(URL_ADMIN));
 	}
 
-	@AfterMethod
+	@AfterTest
 	public void end() {
 		driver.quit();
 	}
